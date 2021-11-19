3c40e9cd99ccb5a81b1102d4736e4b4201c01fd8
thomas <hello@thomasflorentin.net>
2021-11-19 11:19:42 +0100

    plugin: Add Samois plugin

---
+++ htdocs/wp-content/plugins/samois_plugin/acf.php
@@ -0,0 +1,47 @@
+<?php
+
+function mep_acf_load_json($paths) {
+	$paths = array(
+		MEP_DIR.'/acf-json'
+	);
+
+	return $paths;
+}
+
+function mep_acf_save_json($paths) {
+
+	$paths = MEP_DIR.'/acf-json';
+
+	return $paths;
+}
+
+
+add_filter('acf/settings/save_json', 'mep_acf_save_json');
+add_filter('acf/settings/load_json', 'mep_acf_load_json');
+
+add_filter('acf/save_post', function($post_id) {
+	$format = function(&$date) {
+		$tmp = sanitize_text_field($date);
+		if(!empty($tmp)) {
+			preg_match('~(\d{4})(\d{2})(\d{2})~', $tmp, $match);
+			array_shift($match);
+			$date = implode('-', $match);
+		}
+	};
+
+	$format($_POST['acf']['field_58eb82d838d59']);
+	$format($_POST['acf']['field_58eb835538d5a']);
+}, 1, 1);
+
+
+
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' => 'options',
+		'menu_title' => 'Options',
+		'icon_url' => 'dashicons-layout',
+		'position' => 2
+	));
+
+}
---
+++ htdocs/wp-content/plugins/samois_plugin/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Samois sur Seine Plugin
+Author: Thomas Florentin
+Version: 1.0
+*/
+
+define('SAMOIS_DIR', WP_PLUGIN_DIR.'/samois_plugin');
+define('SAMOIS_PATH', '/'.str_replace(ABSPATH, '', SAMOIS_DIR));
+define('SAMOIS_URL', WP_PLUGIN_URL.'/samois_plugin');
+
+
+require_once(SAMOIS_DIR.'/acf.php');
+
