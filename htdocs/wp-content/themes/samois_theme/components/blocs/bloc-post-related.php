7dfd5ce245a0068c46312d0aac76fb76111160f0
Tom Bost <tom.bost.pro@gmail.com>
2022-01-18 11:00:52 +0100

    update(Related Post): Responsive Mob

--- htdocs/wp-content/themes/samois_theme/components/blocs/bloc-post-related.php
+++ htdocs/wp-content/themes/samois_theme/components/blocs/bloc-post-related.php
@@ -1,4 +1,4 @@
-<li class="m_4col item_related">
+<li class="s_12col m_4col item_related">
     <a href="<?php the_permalink() ?>" rel="bookmark" title="<?= get_the_title(); ?>">
         <p class="FS14_U item_related_type mb-vsmall"><?= get_field('type'); ?></p>
         <p class="FS16_B mb-small item_related_title"><?= get_the_title(); ?></p>
