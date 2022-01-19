3f625f423ecf9fa8865a55cc6df29dde39e195fd
Tom Bost <tom.bost.pro@gmail.com>
2022-01-19 16:51:41 +0100

    update(Structure infos): Styles css

--- htdocs/wp-content/themes/samois_theme/components/modules/module-horaires.php
+++ htdocs/wp-content/themes/samois_theme/components/modules/module-horaires.php
@@ -9,17 +9,17 @@
     <div>
 
         <h3 class="h3">Mairie de Samois</h3>
+        <p><?php the_field('structure_adress', 'options') ?></p>
         <p>Horaires d'ouverture au public <br>
             Lundi > Samedi de 8h30 à 16h30</p>
-        <p><?php the_field('structure_adress', 'options') ?></p>
         <p> Courriel : <br> <?php the_field('mail', 'options') ?></p>
 
-        <p>Std : <?php the_field('Phone', 'options') ?></p>
+        <p class="mb-none">Std : <?php the_field('Phone', 'options') ?></p>
         <p>Télécopie : <?php the_field('telecopie', 'options') ?></p>
 
-        <p class="btn_inline">tout les services et contacts de la mairie</p>
+        <a href="/contact" class="btn_inline mb-medium">tout les services et contacts de la mairie</a>
 
-        <p class="mb-small">Newsletter</p>
+        <p class="mb-vsmall">Newsletter :</p>
         
         <form class="form-newsletter field_default" method="get" action="">
             <input type="text" class="search-field" name="s" placeholder="Votre email">
