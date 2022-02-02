d801af288f95fef98e549488be1c25384c082fa9
thomas <hello@thomasflorentin.net>
2022-02-02 14:08:53 +0100

    feat: Polish

--- htdocs/wp-content/themes/samois_theme/components/blocs/bloc-post-related.php
+++ htdocs/wp-content/themes/samois_theme/components/blocs/bloc-post-related.php
@@ -1,7 +1,7 @@
 <?php 
 $excerpt = get_the_excerpt();
  
-$excerpt = substr($excerpt, 0, 260);
+$excerpt = substr($excerpt, 0, 180);
 $excerpt = substr($excerpt, 0, strrpos($excerpt, ' ')) . ' [...]';
 ?>
 
