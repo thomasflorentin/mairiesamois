f07662d78f6a70cb0355b410e4528f3509ed6316
thomas <hello@thomasflorentin.net>
2022-06-15 08:40:22 +0200

    feat: Add Cookies Compl.

--- htdocs/wp-content/themes/samois_theme/header.php
+++ htdocs/wp-content/themes/samois_theme/header.php
@@ -25,6 +25,57 @@ $mail = get_field('mail', 'option');
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
     <?php wp_head(); ?>
+
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/tarteaucitron/tarteaucitron.js"></script>
+
+<script type="text/javascript">
+tarteaucitron.init({
+  "privacyUrl": "", /* Privacy policy url */
+  "bodyPosition": "bottom", /* or top to bring it as first element for accessibility */
+
+  "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+  "cookieName": "tarteaucitron", /* Cookie name */
+
+  "orientation": "bottom", /* Banner position (top - bottom) */
+
+  "groupServices": false, /* Group services by category */
+                   
+  "showAlertSmall": false, /* Show the small banner on bottom right */
+  "cookieslist": false, /* Show the cookie list */
+                   
+  "closePopup": false, /* Show a close X on the banner */
+
+  "showIcon": true, /* Show cookie icon to manage cookies */
+  //"iconSrc": "", /* Optionnal: URL or base64 encoded image */
+  "iconPosition": "BottomRight", /* BottomRight, BottomLeft, TopRight and TopLeft */
+
+  "adblocker": false, /* Show a Warning if an adblocker is detected */
+                   
+  "DenyAllCta" : true, /* Show the deny all button */
+  "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+  "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+                   
+  "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+  "removeCredit": false, /* Remove credit link */
+  "moreInfoLink": true, /* Show more info link */
+
+  "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+  "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
+
+  //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+                  
+  "readmoreLink": "", /* Change the default readmore link */
+
+  "mandatory": true, /* Show a message about mandatory cookies */
+  "mandatoryCta": true /* Show the disabled accept button when mandatory on */
+});
+tarteaucitron.user.googletagmanagerId = 'GTM-XXXX';
+(tarteaucitron.job = tarteaucitron.job || []).push('googletagmanager');
+(tarteaucitron.job = tarteaucitron.job || []).push('youtube');
+</script>
+
+
 </head>
 <?php
 if (is_admin_bar_showing()) : ?>
