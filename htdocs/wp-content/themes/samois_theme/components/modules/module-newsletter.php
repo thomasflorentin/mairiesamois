b83887ed1337dc87f540bdfd28c0400b1bf07de0
thomas <hello@thomasflorentin.net>
2023-01-05 14:48:25 +0100

    newsletter: Change form action

--- htdocs/wp-content/themes/samois_theme/components/modules/module-newsletter.php
+++ htdocs/wp-content/themes/samois_theme/components/modules/module-newsletter.php
@@ -1,25 +1,22 @@
 <p class="mb-small">Recevez notre Infolettre</p>
 
-    <!-- Begin Mailchimp Signup Form -->
+<!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
 
-    <div id="mc_embed_signup">
+    <div id="mce-responses" class="clear foot">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>  
 
-        <div id="mce-responses" class="clear foot">
-            <div class="response" id="mce-error-response" style="display:none"></div>
-            <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>
+    <form action="https://samois-sur-seine.us20.list-manage.com/subscribe/post?u=850f553ee274577ddd60bff24&amp;id=0cda57ed6a&amp;f_id=00b02fe8f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate field_default" target="_self">
 
-        <div id="mc_embed_signup_scroll">
-
+        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="mon.email@domain.fr" required>
+        <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+            <i class="fas fa-arrow-right"></i>
+        </button>
 
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_850f553ee274577ddd60bff24_0cda57ed6a" tabindex="-1" value=""></div>
-
-        </div>
-
-    <form action="https://samois-sur-seine.us20.list-manage.com/subscribe/post?u=850f553ee274577ddd60bff24&amp;id=0cda57ed6a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-newsletter field_default" target="_blank" novalidate>
-        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="mon_email@exemple.com">
-        <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"><i class="fas fa-arrow-right"></i></button>
 
     </form>
 </div>
