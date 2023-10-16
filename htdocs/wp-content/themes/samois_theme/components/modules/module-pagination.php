09c6a2cfdc80298ebb62920f68b8f8cb55e89548
thomas <hello@thomasflorentin.net>
2023-10-16 11:36:48 +0200

    polish

--- htdocs/wp-content/themes/samois_theme/components/modules/module-pagination.php
+++ htdocs/wp-content/themes/samois_theme/components/modules/module-pagination.php
@@ -1,11 +1,11 @@
 
 
-    <nav class="page_navigation" style="font-size: 3rem;">
-        <div class="">
+    <nav class="page_navigation" style="font-size: 2rem;">
+        <div class="" style="display: flex; justify-content: space-between;">
              <?php 
                 
-                next_posts_link( '<- Pages précédentes', $query->max_num_pages );
-                previous_posts_link( 'Pages suivantes ->' ); 
+                next_posts_link( '<- Pages précédentes ', $query->max_num_pages );
+                previous_posts_link( ' Pages suivantes ->' ); 
                 
                 ?>
         </div>
