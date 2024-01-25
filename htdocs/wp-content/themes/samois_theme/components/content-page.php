c08365b5bd8ca1ff2d54aba813b14ad3b4e48a06
thomas <hello@thomasflorentin.net>
2024-01-25 17:24:38 +0100

    styles: Fix layout

--- htdocs/wp-content/themes/samois_theme/components/content-page.php
+++ htdocs/wp-content/themes/samois_theme/components/content-page.php
@@ -12,144 +12,149 @@ if( $vertical_thumbnail === null) {
 get_template_part('components/modules/module', 'breadcrumbs'); ?>
 
 
-<article class="single_post">
+<div>
 
-    <?php if ($imgs) : ?>
+    <article class="single_post">
 
-        <div class="grid mb-big single_cover">
-            <figure class="s_12col m_7col m_full">
-                <div class="cover_slide">
-                    <?php foreach ($imgs as $img) : ?>
-                        <img src="<?= $img['url'] ?>" alt="<?= $img['alt'] ?>" class="">
-                    <?php endforeach; ?>
-                </div>
-            </figure>
+        <?php if ($imgs) : ?>
 
-            <div class="s_12col m_5col mt-big container-title-post p-small">
-                <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
-                
-                <?php if( get_post_type() == 'event' ) : ?>
-
-                    <div class="event_meta mb-big">
-                        <p class="FS24_B mb-small">
-                            <?php get_event_dates(get_the_ID()); ?>
-                        </p>
-                        <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
+            <div class="grid mb-big single_cover">
+                <figure class="s_12col m_7col m_full">
+                    <div class="cover_slide">
+                        <?php foreach ($imgs as $img) : ?>
+                            <img src="<?= $img['url'] ?>" alt="<?= $img['alt'] ?>" class="">
+                        <?php endforeach; ?>
                     </div>
+                </figure>
 
-                <?php endif; ?>
-
-                <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
-            </div>
-        </div>
-
-
-    <?php elseif ( $vertical_thumbnail && has_post_thumbnail() ) : ?>
-
-        <div class="grid mb-big single_cover thumbnail_single vertical_image">
-            <figure class="s_12col m_5col m_full">
-                <div class="inner">
-                    <?php the_post_thumbnail(); ?>
-                </div>
-            </figure>
-
-            <div class="s_12col m_7col mt-big container-title-post  p-small">
-                <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
-
-                <?php if( get_post_type() == 'event' ) : ?>
-
-                    <div class="event_meta mb-big">
-                        <p class="FS24_B mb-small">
-                            <?php get_event_dates(get_the_ID()); ?>
-                        </p>
-                        <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
-                    </div>
-
-                <?php endif; ?>
-
-                <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
-            </div>
-        </div>
-
-    <?php elseif ( !$vertical_thumbnail && has_post_thumbnail() ) : ?>
-
-        <div class="grid mb-big single_cover thumbnail_single">
-            <figure class="s_12col m_7col m_full">
-                <div class="inner">
-                    <?php the_post_thumbnail('single-main'); ?>
-                </div>
-            </figure>
-
-            <div class="s_12col m_5col mt-big container-title-post p-small">
-                <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
-                <?php if( get_post_type() == 'event' ) : ?>
-
-                    <div class="event_meta mb-big">
-                        <p class="FS24_B mb-small">
-                            <?php get_event_dates(get_the_ID()); ?>
-                        </p>
-                        <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
-                    </div>
-
-                <?php endif; ?>
-                <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
-            </div>
-        </div>
-
-    <?php else : // else imgs 
-
-        if (is_page()) : ?>
-            <div class="grid mb-big">
-                <div class="s_12col m_7col col_start_2">
-                    <h1 class="FS42_B"><?php the_title(); ?></h1>
-                    <div class="FS18"><?php the_excerpt(); ?></div>
-                </div>
-            </div>
-
-        <?php else : ?>
-            <div class="grid mb-big">
-                <div class="s_12col m_7col col_start_2">
-                    <h1 class="FS42_B"><?php the_title(); ?></h1>
+                <div class="s_12col m_5col mt-big container-title-post p-small">
+                    <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
+                    
                     <?php if( get_post_type() == 'event' ) : ?>
 
                         <div class="event_meta mb-big">
-                            <p class="FS24_B mb-small">            
+                            <p class="FS24_B mb-small">
                                 <?php get_event_dates(get_the_ID()); ?>
                             </p>
                             <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
                         </div>
 
                     <?php endif; ?>
-                    <div class="FS18_B"><?php the_excerpt(); ?></div>
+
+                    <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
                 </div>
             </div>
 
-        <?php endif; ?>
 
-    <?php endif; // if imgs ?>
+        <?php elseif ( $vertical_thumbnail && has_post_thumbnail() ) : ?>
+
+            <div class="grid mb-big single_cover thumbnail_single vertical_image">
+                <figure class="s_12col m_5col m_full">
+                    <div class="inner">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                </figure>
+
+                <div class="s_12col m_7col mt-big container-title-post  p-small">
+                    <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
+
+                    <?php if( get_post_type() == 'event' ) : ?>
+
+                        <div class="event_meta mb-big">
+                            <p class="FS24_B mb-small">
+                                <?php get_event_dates(get_the_ID()); ?>
+                            </p>
+                            <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
+                        </div>
+
+                    <?php endif; ?>
+
+                    <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
+                </div>
+            </div>
+
+        <?php elseif ( !$vertical_thumbnail && has_post_thumbnail() ) : ?>
+
+            <div class="grid mb-big single_cover thumbnail_single">
+                <figure class="s_12col m_7col m_full">
+                    <div class="inner">
+                        <?php the_post_thumbnail('single-main'); ?>
+                    </div>
+                </figure>
+
+                <div class="s_12col m_5col mt-big container-title-post p-small">
+                    <h1 class="FS42_B post-title"><?php the_title(); ?></h1>
+                    <?php if( get_post_type() == 'event' ) : ?>
+
+                        <div class="event_meta mb-big">
+                            <p class="FS24_B mb-small">
+                                <?php get_event_dates(get_the_ID()); ?>
+                            </p>
+                            <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
+                        </div>
+
+                    <?php endif; ?>
+                    <div class="FS16_B post-excerpt"><?php the_excerpt(); ?></div>
+                </div>
+            </div>
+
+        <?php else : // else imgs 
+
+            if (is_page()) : ?>
+                <div class="grid mb-big">
+                    <div class="s_12col m_7col col_start_2">
+                        <h1 class="FS42_B"><?php the_title(); ?></h1>
+                        <div class="FS18"><?php the_excerpt(); ?></div>
+                    </div>
+                </div>
+
+            <?php else : ?>
+                <div class="grid mb-big">
+                    <div class="s_12col m_7col col_start_2">
+                        <h1 class="FS42_B"><?php the_title(); ?></h1>
+                        <?php if( get_post_type() == 'event' ) : ?>
+
+                            <div class="event_meta mb-big">
+                                <p class="FS24_B mb-small">            
+                                    <?php get_event_dates(get_the_ID()); ?>
+                                </p>
+                                <p class="FS24_B txt-capitalize"><?php the_field('location'); ?></p>
+                            </div>
+
+                        <?php endif; ?>
+                        <div class="FS18_B"><?php the_excerpt(); ?></div>
+                    </div>
+                </div>
+
+            <?php endif; ?>
+
+        <?php endif; // if imgs ?>
 
 
-    <div class="grid">
-        <div class="s_12col m_8col copy FS16">
-            <?= do_shortcode(the_content());  ?>
+        <div class="grid">
+            <div class="s_12col m_8col copy FS16">
+                <?= do_shortcode(the_content());  ?>
 
-            <?php get_template_part('components/content', 'flexible'); ?>
+                <?php get_template_part('components/content', 'flexible'); ?>
 
+            </div>
         </div>
-    </div>
 
 
 
-</article>
+    </article>
 
 
-<?php
+    <?php
 
-$tags = wp_get_post_tags(
-    $post->ID,
-    array(
-        'fields' => 'ids',
-    )
-);
-set_query_var('tags', $tags);
-get_template_part('components/related-post'); ?>
+    $tags = wp_get_post_tags(
+        $post->ID,
+        array(
+            'fields' => 'ids',
+        )
+    );
+    set_query_var('tags', $tags);
+    get_template_part('components/related-post'); ?>
+
+
+</div>
