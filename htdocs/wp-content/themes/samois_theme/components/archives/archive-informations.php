e0db3bf033d3f11bc07d80a2388659d4b7b18cb0
thomas <hello@thomasflorentin.net>
2024-01-25 17:28:34 +0100

    styles: Fix layout

--- htdocs/wp-content/themes/samois_theme/components/archives/archive-informations.php
+++ htdocs/wp-content/themes/samois_theme/components/archives/archive-informations.php
@@ -6,46 +6,49 @@
 ?>
 
 
+<div>
 
-<header class="mb-medium grid">
-    <div class="s_12col m_9col">
-        <h1 class="FS42_B"><?= the_title() ?></h1>
-        <div class="FS14"><?= the_excerpt() ?></div>
-    </div>
-</header>
-
-
-
-<section class="module mb-big">
-    <?php 
-        set_query_var('before', true);
-        get_template_part('components/content', 'flexible'); ?>
-</section>
+    <header class="mb-medium grid">
+        <div class="s_12col m_9col">
+            <h1 class="FS42_B"><?= the_title() ?></h1>
+            <div class="FS14"><?= the_excerpt() ?></div>
+        </div>
+    </header>
 
 
 
-<section class="module mb-big">
-
-    <?php if( get_field("module_title") !== "" ) { 
-        echo '<header class="mb-small"><h2 class="FS42_BC ">'. get_field("module_title") . '</h2></header>';
-    } ?>
-
-    <div class="grid">
-
-        <?php
-        foreach ($page_children as $child) {
-            set_query_var('parent_id', $child->ID);
-            get_template_part('components/modules/module', 'informations');
-        }
-        ?>
-
-    </div>
-</section>
-
-
-
-<section class="module mb-big">
-    <?php 
-            set_query_var('before', false);
+    <section class="module mb-big">
+        <?php 
+            set_query_var('before', true);
             get_template_part('components/content', 'flexible'); ?>
-</section>
+    </section>
+
+
+
+    <section class="module mb-big">
+
+        <?php if( get_field("module_title") !== "" ) { 
+            echo '<header class="mb-small"><h2 class="FS42_BC ">'. get_field("module_title") . '</h2></header>';
+        } ?>
+
+        <div class="grid">
+
+            <?php
+            foreach ($page_children as $child) {
+                set_query_var('parent_id', $child->ID);
+                get_template_part('components/modules/module', 'informations');
+            }
+            ?>
+
+        </div>
+    </section>
+
+
+
+    <section class="module mb-big">
+        <?php 
+                set_query_var('before', false);
+                get_template_part('components/content', 'flexible'); ?>
+    </section>
+
+</div>
