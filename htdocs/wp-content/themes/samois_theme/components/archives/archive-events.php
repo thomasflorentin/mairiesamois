76bfcb17a59abda58bf973f59d848cfe50b2c9a4
Thomas <hello@thomasflorentin.net>
2024-06-27 19:14:59 +0200

    Update archive-events.php

--- htdocs/wp-content/themes/samois_theme/components/archives/archive-events.php
+++ htdocs/wp-content/themes/samois_theme/components/archives/archive-events.php
@@ -2,73 +2,75 @@
     <?php get_template_part('components/shortcuts' ); ?>
 
 
-    <header>
-        <h1 class="FS42_B"><?= the_title() ?></h1>
-        <div class="grid mb-big copy">
-            <div class="s_12col m_9col">
-                <p class="FS14"><?= get_the_excerpt() ?></p>
+    <section>
+        <header>
+            <h1 class="FS42_B"><?= the_title() ?></h1>
+            <div class="grid mb-big copy">
+                <div class="s_12col m_9col">
+                    <p class="FS14"><?= get_the_excerpt() ?></p>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-        
-    <ul class="agenda-list">
-        
-        <?php
-        $current_header = '';
-        setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
-        $today = date('Ymd');
-
-        $args = array(
-            'post_type'         => 'event',
-            'posts_per_page' 	=> '-1',
-            'meta_key'          => 'date',
-            'orderby'           => 'meta_value_num',
-            'order'             => 'ASC',
-            'meta_query'        => array(
-                array(
-                    'key'           => 'date',
-                    'value'         => $today,
-                    'compare'       => '>=',
-                )
-            )
-        );
-        $allevents = new WP_Query($args);
-        $i = 0;
-
-        if ( $allevents->have_posts() ) : 
-
-            echo '<div class="agenda-groupe mb-big">';
-
-            while ( $allevents->have_posts() ) : $allevents->the_post(); 
-
-                $date = get_field('date', false, false);
-                $temp_header = date_i18n('F Y', strtotime($date));
-
-                if ($temp_header != $current_header) {
-                    $current_header = $temp_header;
-                    if( $i !== 0 ) {
-                        echo '</div>';
-                        echo '<div class="agenda-groupe mb-big">';
-                    }
-                    echo "<h3 class='agenda-month FS24_B txt-capitalize mb-vsmall'>" . $current_header . '</h3>';
-                } ?>
-
-                <?php get_template_part('components/blocs/bloc', 'event'); ?>
-
-            <?php
-                $i++; 
-                endwhile; 
-                echo '</div>'; 
-                ?>
             
+        <ul class="agenda-list">
+            
+            <?php
+            $current_header = '';
+            setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
+            $today = date('Ymd');
 
+            $args = array(
+                'post_type'         => 'event',
+                'posts_per_page' 	=> '-1',
+                'meta_key'          => 'date',
+                'orderby'           => 'meta_value_num',
+                'order'             => 'ASC',
+                'meta_query'        => array(
+                    array(
+                        'key'           => 'date_fin',
+                        'value'         => $today,
+                        'compare'       => '>=',
+                    )
+                )
+            );
+            $allevents = new WP_Query($args);
+            $i = 0;
 
-            <?php 
-                set_query_var('query', $allevents);
-                get_template_part('components/modules/module', 'pagination'); ?>
+            if ( $allevents->have_posts() ) : 
+
+                echo '<div class="agenda-groupe mb-big">';
+
+                while ( $allevents->have_posts() ) : $allevents->the_post(); 
+
+                    $date = get_field('date', false, false);
+                    $temp_header = date_i18n('F Y', strtotime($date));
+
+                    if ($temp_header != $current_header) {
+                        $current_header = $temp_header;
+                        if( $i !== 0 ) {
+                            echo '</div>';
+                            echo '<div class="agenda-groupe mb-big">';
+                        }
+                        echo "<h3 class='agenda-month FS24_B txt-capitalize mb-vsmall'>" . $current_header . '</h3>';
+                    } ?>
+
+                    <?php get_template_part('components/blocs/bloc', 'event'); ?>
+
+                <?php
+                    $i++; 
+                    endwhile; 
+                    echo '</div>'; 
+                    ?>
                 
 
-        <?php endif;  wp_reset_postdata();  ?>
-        
-    </ul>
+
+                <?php 
+                    set_query_var('query', $allevents);
+                    get_template_part('components/modules/module', 'pagination'); ?>
+                    
+
+            <?php endif;  wp_reset_postdata();  ?>
+            
+        </ul>
+        </section>
