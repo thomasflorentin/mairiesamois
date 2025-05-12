72a518ca7c66ef621a1855e1e84480d0c7b007ab
Thomas <hello@thomasflorentin.net>
2025-05-12 08:40:41 +0200

    updates: Yoast SEO

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5f4a5446ea71a232d9b78e343286134d::getLoader();
+return ComposerAutoloaderInit18ea696e1cd589184e616540b92e0296::getLoader();
